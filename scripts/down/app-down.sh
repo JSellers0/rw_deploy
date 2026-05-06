fc305d1a5fd971f36b2b5730a1edf92225d42cf8
JSellers0@mainframe <rw-admin.unruly589@passinbox.com>
2026-05-06 07:32:30 -0400

    fix container names

--- scripts/down/app-down.sh
+++ scripts/down/app-down.sh
@@ -10,4 +10,4 @@ echo " Tearing Down Rock Willow Applications"
 echo "======================================="
 
 podman-compose -f app-compose.yml stop 
-podman rm rw_app_budget_api rw_app_budget
+podman rm rw-app-budget-api rw-app-budget
